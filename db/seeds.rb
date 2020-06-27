f41c4edfd53b43698d71464e91443ac1df2a7bc3
Yuri Gravatá <ygravata@gmail.com>
2020-06-27 13:46:08 -0300

    creating a simple seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: 'ygravata@gmail.com', password: "testtest")
+
+PartialSale.create!(user_id: 1)
+
+5.times do
+    Sale.create!(
+        purchase_name: "João Silva",
+        item_description: "R$10 off R$20 of food",
+        item_price: 10.0,
+        purchase_count: 2,
+        merchant_address: "987 Fake St",
+        merchant_name: "Bob's, Pizza",
+        partial_sale_id: 1
+    ) 
+end
