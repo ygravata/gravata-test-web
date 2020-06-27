839be5e06dcc36c146356d55c8dc7f245bc7eb11
Yuri Gravatá <ygravata@gmail.com>
2020-06-27 12:21:47 -0300

    Adding controllers and routes

---
+++ app/controllers/partial_sales_controller.rb
@@ -0,0 +1,2 @@
+class PartialSalesController < ApplicationController
+end
---
+++ test/controllers/partial_sales_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PartialSalesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
