3e4472617608bfab7457e6edc779ec151ba1201e
Yuri Gravatá <ygravata@gmail.com>
2020-06-28 00:27:29 -0300

    parsing solved

--- app/controllers/company_partial_sales_controller.rb
+++ app/controllers/company_partial_sales_controller.rb
@@ -1,14 +1,47 @@
 class CompanyPartialSalesController < ApplicationController
-    def index
-        @company_partial_sales = CompanyPartialSale.all
-        @all = Sale.all
-        
-        @total = 0
+  require 'csv'
 
-        @all.each do |sale|
-            @total += sale.total_income
-        end
-        @total
+  def index
+    @company_partial_sale = CompanyPartialSale.new
+    @company_partial_sales = CompanyPartialSale.all.order(created_at: :desc)
+    @all = Sale.all  
+    @total = 0
+
+    @all.each do |sale|
+        @total += sale.total_income
     end
+  end
+
+  def new
+    @company_partial_sale = CompanyPartialSale.new      
+  end
+  
+  def create
+    @company_partial_sale = CompanyPartialSale.new
+    @company_partial_sale.user = current_user
+    
+    if @company_partial_sale.save!
+      if params[:company_partial_sale][:dataset]
+
+        csv_options = { col_sep: "\t", quote_char: '"', headers: :first_row }
+        filepath = params[:company_partial_sale][:dataset]
+               
+        CSV.foreach(filepath, csv_options) do |row|
+          new_sale = Sale.new(purchase_name: row[0].to_s, item_description: row[1].to_s, item_price: row[2].to_i, purchase_count: row[3].to_i, merchant_address: row[4].to_s, merchant_name: row[5].to_s)
+          new_sale.company_partial_sale_id = @company_partial_sale.id
+          new_sale.save!                  
+        end
+      end
+      redirect_to company_partial_sales_path, notice: "Upload sucessfull realized!"
+    else
+      render :new, notice: "Please, try again."
+    end 
+  end
+
+	private
+
+	def company_partial_sale_params
+		params.require(:company_partial_sale).permit(:dataset)
+	end
     
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning database...'
+require 'csv'
 
-CompanyPartialSale.destroy_all
-Sale.destroy_all
-User.destroy_all
+# CompanyPartialSale.destroy_all
+# Sale.destroy_all
+# User.destroy_all
 
-puts "Creating a user"
-new_user = User.create!(email: 'ygravata@gmail.com', password: "testtest")
-puts "User created!"
-
-
-company_seed = CompanyPartialSale.create!(user_id: new_user.id)
+# puts "Creating a user"
+# new_user = User.create!(email: 'ygravata@gmail.com', password: "testtest")
+# puts "User created!"
 
 
-5.times do
-    new_sale = Sale.create!(
-        purchase_name: "João Silva",
-        item_description: "R$10 off R$20 of food",
-        item_price: 10.0,
-        purchase_count: 2,
-        merchant_address: "987 Fake St",
-        merchant_name: "Bob's, Pizza",
-        company_partial_sale_id: company_seed.id
-    )
-    puts "Sale #{new_sale.id} created" 
-end
+# company_seed = CompanyPartialSale.create!(user_id: new_user.id)
+
+
+# 5.times do
+#     new_sale = Sale.create!(
+#         purchase_name: "João Silva",
+#         item_description: "R$10 off R$20 of food",
+#         item_price: 10.0,
+#         purchase_count: 2,
+#         merchant_address: "987 Fake St",
+#         merchant_name: "Bob's, Pizza",
+#         company_partial_sale_id: company_seed.id
+#     )
+#     puts "Sale #{new_sale.id} created" 
+# end
+
+        csv_options = { col_sep: "\t", quote_char: '"', headers: :first_row }
+        # filepath = params[:company_partial_sale][:dataset]
+        filepath = ('storage/example_input.tab')
+         
+    CSV.foreach(filepath, csv_options) do |row|
+      new_sale = Sale.new(purchase_name: row[0].to_s, item_description: row[1].to_s, item_price: row[2].to_i, purchase_count: row[3].to_i, merchant_address: row[4].to_s, merchant_name: row[5].to_s)
+      new_sale.company_partial_sale_id = CompanyPartialSale.last.id
+      new_sale.save!                  
+    end
